7e640f9dc14a92adba75ae43a59c8800db0a570d
Nick Nichols <nichols1991@gmail.com>
2024-03-10 23:28:05 +0000

    Leave changelog note regarding recent clojure CVE

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.wallbrew/common-beer-data "1.3.0"
+(defproject com.wallbrew/common-beer-data "1.3.1"
   :description "A repository of common-beer-format data."
   :url "https://github.com/Wall-Brew-Co/common-beer-data"
   :license {:name         "MIT"
